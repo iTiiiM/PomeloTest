68b338dfbdae3cdc13b6168c72bbb856e01b5963
Piyatat Thianboonsong <mewloft@gmail.com>
2020-08-17 15:16:17 +0700

    Add StoreLocatioCell

---
+++ PomeloTest/StoreLocationCell.swift
@@ -0,0 +1,35 @@
+//
+//  StoreLocationCell.swift
+//  PomeloTest
+//
+//  Created by Piyatat  Thianboonsong on 17/8/2563 BE.
+//  Copyright © 2563 Piyatat  Thianboonsong. All rights reserved.
+//
+
+import UIKit
+
+enum DistanceMetrics: String {
+    case kilometer = "Km"
+}
+
+class StoreLocationCell: UITableViewCell {
+
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var aliasLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var distanceFromCurrentLocationLabel: UILabel!
+    @IBOutlet weak var distanceMetricsLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
